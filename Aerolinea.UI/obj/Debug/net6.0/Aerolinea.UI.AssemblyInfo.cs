363fb9170da1dc852b023d215479819f495bc81d
Gerardo Romero <gerardo.rom280@gmail.com>
2024-11-25 10:59:05 -0600

    Segundo commit

--- Aerolinea.UI/obj/Debug/net6.0/Aerolinea.UI.AssemblyInfo.cs
+++ Aerolinea.UI/obj/Debug/net6.0/Aerolinea.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aerolinea.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e21c9410ba91caa5990cc3b4c54b74372b0d44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aerolinea.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aerolinea.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
